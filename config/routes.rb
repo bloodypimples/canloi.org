1166df909219fc79687cdc3b7749776afc69a7bb
Hieu <hieudoan2609@gmail.com>
2017-09-15 18:39:37 +0700

    www redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   root 'fuckyou#index'
+  
+  constraints(host: /^www\.(?!cute\.)/i) do
+
+    match '(*any)', via: :all, to: redirect { |params, request|
+
+      # parse the current request url
+      # tap in and remove www.
+      URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '') }.to_s
+
+    }
+
+  end
 end
