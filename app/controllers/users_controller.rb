822e85d6c21566f658808c6e147b03125d852508
Hieu <hieudoan2609@gmail.com>
2017-09-18 10:11:40 +0700

    show page posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id).order("created_at desc")
   end
 end
