24106ce00645ab44fd3c92943fbb5417c8843390
Hieu <hieudoan2609@gmail.com>
2017-09-19 10:12:58 +0700

    add structure & styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts
+  has_many :posts, dependent: :destroy
   # only returns user's name if that exists, otherwise returns user's email
   def get_name
-    if self.name.empty?
+    if self.name.nil? || self.name.empty?
       self.email
     else
       self.name
